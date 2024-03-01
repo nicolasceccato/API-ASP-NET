c49e8db64078d93ac848fc77dd635ba5f43849b1
Nicolas Gabriel Ceccato <nicolasceccato@hotmail.com>
2024-02-29 21:14:19 -0300

    update(project) - initial commit

---
+++ MeuTodo/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using MeuTodo.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MeuTodo.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) => optionsBuilder.UseSqlite("DataSource=app.db;Cache=Shared");
+    }
+}
---
+++ MeuTodo/Models/Todo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MeuTodo.Models
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public String Title { get; set; }
+        public bool Done { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
